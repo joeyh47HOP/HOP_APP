9e324438c532cdc05aef7c3d7f22b0725c0a0dec
Joseph <joseph.hwang@hourofpower.org>
2025-06-11 17:03:58 -0700

    first

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+"import 'react-native-gesture-handler';"
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import PrayerScreen from './screens/PrayerScreen';
